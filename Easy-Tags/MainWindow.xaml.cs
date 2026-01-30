e017c179449be72a1669edb5e7d89f4a48e2a3b2
Tim Owen <Tim.Owen@zenger.com>
2026-01-30 12:02:07 -0500

    Add project files.

---
+++ Easy-Tags/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Management;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Printing.IndexedProperties;
+using System.Printing;
+
+namespace Easy_Tags
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string TargetPrinter;
+        public MainWindow()
+        {
+            InitializeComponent();
+            TargetPrinter = GetDefaultPrintQueue();
+            PrinterName.Content = TargetPrinter;
+        }
+
+        private string GetDefaultPrintQueue()
+        {
+            LocalPrintServer lpServer = new LocalPrintServer();
+            PrintQueue printQueue = lpServer.DefaultPrintQueue;
+            return printQueue.FullName;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int startTag = 0, endTag = 0;
+            string job, counts, stream, description;
+            if (JobNumber.Text == String.Empty) job = "--";
+            else job = JobNumber.Text;
+            if (Description.Text == String.Empty) description = "--";
+            else description = Description.Text;
+            if (Int32.TryParse(StartTag.Text, out startTag) && Int32.TryParse(EndTag.Text, out endTag))
+            {
+                if(description.Length <= 40)
+                {
+                    if ((bool)ReversePrint.IsChecked)
+                    {
+                        for (int i = endTag; i >= startTag; i--)
+                        {
+                            counts = ((bool)IncludeEnd.IsChecked) ? String.Format("{0}/{1}", i.ToString("D3"), endTag.ToString("D3")) : i.ToString("D3");
+                            stream = RawPrinterHelper.GetTrayTag(counts, job, description);
+                            if (!RawPrinterHelper.SendStringToPrinter(TargetPrinter, stream))
+                            {
+                                MessageBox.Show("Error communicating with printer.");
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        for(int i = startTag; i <= endTag; i++)
+                        {
+                            counts = ((bool)IncludeEnd.IsChecked) ? String.Format("{0}/{1}", i.ToString("D3"), endTag.ToString("D3")) : i.ToString("D3");
+                            stream = RawPrinterHelper.GetTrayTag(counts, job, description);
+                            if (!RawPrinterHelper.SendStringToPrinter(TargetPrinter, stream))
+                            {
+                                MessageBox.Show("Error communicating with printer.");
+                                break;
+                            }
+                        }
+                    }
+
+                    stream = RawPrinterHelper.GetBreakTag();
+                    RawPrinterHelper.SendStringToPrinter(TargetPrinter, stream);
+                }
+                else
+                {
+                    MessageBox.Show("Description must be 40 or fewer characters.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Tag numbers must be valid integers.");
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            PrintDialog printDialog = new PrintDialog();
+            bool? print = printDialog.ShowDialog();
+            if(print == true)
+            {
+                TargetPrinter = printDialog.PrintQueue.FullName;
+                PrinterName.Content = TargetPrinter;
+            }
+        }
+    }
+}
---
+++ Easy-Tags/TrayTag.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Tags
+{
+    public class TrayTag
+    {
+        public static void GetTrayTagStr(string job, string File_Name, string filenum, string Pack_ID_Start, string Pack_ID_Stop, string tagnumber, out string TagString)
+        {
+            string TrayTagString = "";
+            string CR = Environment.NewLine;
+            Int32 fs = 16;
+
+            if (File_Name.Length > 30)
+            {
+                fs = 14;
+            }
+
+            TrayTagString = TrayTagString + "^D57" + CR +
+            "6,832,381,0,0,7,3,1,332,0,0 " + CR +
+            "1,130,315," + (tagnumber.Length + 5) + ",1,12,0,4,1,1,,,,,0 " + CR +
+            "2,416,290," + job.Length + ",1,16,0,4,1,1,,,,,0 " + CR +
+            "3,416,244," + File_Name.Length + ",1," + fs.ToString() + ",0,4,1,1,,,,,0 " + CR +
+            "4,416,118," + filenum.Length + ",1,20,0,4,2,2,,,,,0 " + CR +
+            "5,416,77," + (Pack_ID_Start.Length + 1) + ",1,16,0,4,1,1,,,,,0 " + CR +
+            "6,416,37," + Pack_ID_Stop.Length + ",1,16,0,4,1,1,,,,,0 " + CR +
+            "^D56 " + CR +
+            "^D2 " + CR +
+            "TAG#-" + tagnumber + CR +
+            job + CR +
+            File_Name + CR +
+            filenum + CR +
+            Pack_ID_Start + "-" + CR +
+            Pack_ID_Stop + CR +
+            "^D3 " + CR;
+
+            TagString = TrayTagString;
+
+        }
+
+    }
+}
